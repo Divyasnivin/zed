76ab142a356269d2e561fc54f4eade7406c8218a
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-07-13 01:58:07 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,13 +3,13 @@ from sample_config import Config
 
 class Development(Config):
     # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي وضع اكوادك
-    APP_ID = 6
-    API_HASH = "ضع كود الايبي هاش"
-    ALIVE_NAME = "اسم حسابك التلي"
-    DB_URI = "رابـط التخـزين الخـاص بك"
-    STRING_SESSION = "كود تيرمــكس"
-    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
-    PRIVATE_GROUP_BOT_API_ID = -100
+    APP_ID = 9365736
+    API_HASH = "3fa1b478ca72a73d267e10188556cc34"
+    ALIVE_NAME = "هادي"
+    DB_URI = "postgres://hbifpcsx:H1KGLnvFlLPFs5u9g6tjhNb9bbdA8zmc@salt.db.elephantsql.com/hbifpcsx"
+    STRING_SESSION = "1BJWap1wBu8MYaAS7Xcbnl4M-qym80Q7WcoTR0c38A_Hf3S3t2qL1dE1vPeT2_FGmwC9n9DV1rQzhdTiQdH-5u4BiCEfbAtUacCwThZK_MK6I0Dib1-fZMpRKK-zChLp9YqRUvu_A2hYGShzq2pgr517mFjqmiqzoZ2R0AferTduQaWrRGPSLf0zFUF0W14HjqRu9IZfQTJcM82mZ7yjEiLqhHRcGF_m-uxcB3Xr-EzNRUOhmkqSdCc2aQC-3KzxMXzZQzlrT_Gpomv8e00xGAcbiyVx1OTtC09mOjOSlBvWPVfnYoHi8D-9ZcJvNyg_1x5QEyAEUNuHJQuhh4jUwsDmQL1JIXrc="
+    TG_BOT_TOKEN = "5002378607:AAGWNjtOU594TQgg_r2P-pW6q1O6JZGcyUw"
+    PRIVATE_GROUP_BOT_API_ID = -1001659591037
     COMMAND_HAND_LER = "."
     SUDO_COMMAND_HAND_LER = "،"
     TZ = "Asia/Baghdad"
